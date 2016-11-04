25a1439f70e8c77b12515dea0f906e74a978c7c2
Mario Alberto Segura Oliva <marioalbertoseguraoliva@gmail.com>
2016-11-04 16:56:54 +0000

    More Changes in DeveloperRoomM, added Door with Teleport into Any Instance

---
+++ FireMonkeys/Assets/EnterInstance.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterInstance : MonoBehaviour {
+
+    public Transform destiny;
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            other.GetComponentInParent<Transform>().transform.position = destiny.position;
+    }
+}
