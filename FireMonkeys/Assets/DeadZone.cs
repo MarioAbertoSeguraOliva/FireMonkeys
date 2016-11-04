21c43c1d4d41fd0bc9fe56bb715de0a1f0973c23
Mario Alberto Segura Oliva <marioalbertoseguraoliva@gmail.com>
2016-11-04 12:52:33 +0000

    DeveloperRoomM changes

---
+++ FireMonkeys/Assets/DeadZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DeadZone : MonoBehaviour {
+
+    public int sceneIndex = 1;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") SceneManager.LoadScene(sceneIndex);
+    }
+}
