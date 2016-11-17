0f3a3cd9ea8d15c3c99e0a101d4d572839431856
Mario Alberto Segura Oliva <marioalbertoseguraoliva@gmail.com>
2016-11-17 12:57:40 +0000

    a

---
+++ FireMonkeys/Assets/EnableCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableCamera : MonoBehaviour {
+
+    public Transform startPoint;
+    public Transform endPoint;
+    bool rendering;
+
+	void Update () {
+        //StartCoroutine(StartRendering());
+	}
+    
+    //mario entra en los colliders de los lados tonto
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Entering Collider1");
+            Camera.main.enabled = false;
+            this.GetComponent<Camera>().enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Exiting ColliderX");
+            this.GetComponent<Camera>().enabled = false;
+            Camera.main.enabled = true;
+        }
+    }
+    //IEnumerator StartRendering()
+   // {
+    //    yield return 0;
+    //}
+}
