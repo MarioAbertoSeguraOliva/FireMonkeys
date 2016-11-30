22421f119d4693679434945e06276f246a5a58d9
alberto <albertoesbueno@hotmail.com>
2016-11-30 12:36:38 +0000

    Health bar
    Added health component to character.
    Also created a health bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Health : MonoBehaviour
+{
+    public delegate void OnChangeHealth(float health);
+    public event OnChangeHealth onChangeHealthEvent;
+
+    public float initHealth = 100;
+    public float maxHealth = 100;
+
+    private float health;
+
+    void Start()
+    {
+        health = initHealth;
+    }
+
+    public float Amount {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = Mathf.Clamp(value, 0, maxHealth);
+            onChangeHealthEvent.Invoke(health);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+            Amount -= 5;
+    }
+}
+
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+    public Health health;
+    private GameObject background;
+    private GameObject bar;
+    private Vector3 barInitialScale;
+
+    // Use this for initialization
+    void Start () {
+        health.onChangeHealthEvent += OnChangeHealth;
+        background = transform.FindChild("Background").gameObject;
+        bar = transform.FindChild("Bar").gameObject;
+        barInitialScale = bar.GetComponent<RectTransform>().localScale;
+    }
+	
+    void OnChangeHealth(float amount)
+    {
+        float maxHealth = health.maxHealth;
+        bar.GetComponent<RectTransform>().localScale = 
+            new Vector3(amount / maxHealth * barInitialScale.x, barInitialScale.y, barInitialScale.z);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    
+}
