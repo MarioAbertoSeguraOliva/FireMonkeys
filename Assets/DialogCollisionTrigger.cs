4e261dd1f595adfb8e33451427a7204150c16a6b
alberto <albertoesbueno@hotmail.com>
2016-12-05 20:59:44 +0000

    Progresion in tutorial
    Imported dialog.
    Created scripts to trigger dthe dialog.
    Changed the scene.

---
+++ Assets/DialogCollisionTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogCollisionTrigger : MonoBehaviour {
+
+    public AudioClip dialogueClip;
+    public string dialogueFile;
+    public bool onlyOnce = true;
+    private bool displayed = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !displayed)
+        {
+            DialogueManager.Instance.BeginDialogue(dialogueFile, dialogueClip);
+            displayed = true;
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public static DialogueManager Instance { get; private set; }
+    private AudioClip dialogueAudio;
+    string[] fileLines;
+    string displaySubtitle;
+    string dialogueFile;
+    float subtitleTime;
+    int line = 0;
+
+    void Awake()
+    {
+        if (Instance != null & Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+
+    public void BeginDialogue(string dialogueFile, AudioClip passedClip)
+    {
+        if(passedClip != null) 
+            playAudio(passedClip);
+        this.dialogueFile = dialogueFile;
+        playSubtitle();
+        subtitleTime = Time.time;
+    }
+
+    void playAudio(AudioClip passedClip)
+    {
+        dialogueAudio = passedClip;
+        GetComponent<AudioSource>().clip = passedClip;
+        GetComponent<AudioSource>().Play();
+    }
+
+    //TextAsset.text shows all lines, for this example fileLines[0] will work
+    void playSubtitle()
+    {
+        TextAsset temp = (TextAsset)Resources.Load("Dialogues/" + dialogueFile);
+        fileLines = temp.text.Split('\n');
+        line = 0;
+        displaySubtitle = fileLines[0];
+        GetComponentInChildren<Text>().text = displaySubtitle;
+        
+    }
+
+    void Update()
+    {
+        if (fileLines != null && Time.time - subtitleTime > 2.5f && line < fileLines.Length)
+        {
+            GetComponentInChildren<Text>().text = fileLines[line++];
+            subtitleTime = Time.time;
+        }
+    }
+}
