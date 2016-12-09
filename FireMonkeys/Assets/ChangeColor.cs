2c977d6fe839660276504529134469527cd03fbc
Mario Alberto Segura Oliva <marioalbertoseguraoliva@gmail.com>
2016-12-09 12:56:34 +0000

    Added MainMenu, just need to add sound to buttons and change images, but works great

---
+++ FireMonkeys/Assets/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ChangeColor : MonoBehaviour {
+
+    public AudioClip passedClip;
+
+    void OnMouseEnter()
+    {
+        GetComponent<AudioSource>().clip = passedClip;
+        
+    }
+
+}
--- FireMonkeys/Assets/SceneLoader.cs
+++ FireMonkeys/Assets/SceneLoader.cs
@@ -6,6 +6,7 @@ public class SceneLoader : MonoBehaviour {
 
     public int SceneIndexToLoad;
     public GameObject ObjectToMove;
+    public GameObject loadingImage;
 
 	void OnTriggerEnter(Collider other)
     {
@@ -14,5 +15,11 @@ public class SceneLoader : MonoBehaviour {
             SceneManager.MoveGameObjectToScene(ObjectToMove, SceneManager.GetSceneAt(SceneIndexToLoad));
             SceneManager.LoadScene(SceneIndexToLoad);
         }
+    }
+
+    public void LoadScene(int SceneIndexToLoad)
+    {
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(SceneIndexToLoad);
     }
 }
