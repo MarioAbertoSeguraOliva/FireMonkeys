22ed3a75493a3d24515fb1216f5b6759ccb42b6e
alberto <albertoesbueno@hotmail.com>
2016-12-16 16:44:12 +0000

    Improved menu & camera

---
+++ FireMonkeys/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ChangeColor : MonoBehaviour {
+
+    public AudioClip passedClip;
+
+    public void OnMouseEnter()
+    {
+        GameObject.FindGameObjectWithTag("BackgroundMusic").GetComponent<AudioSource>().PlayOneShot(passedClip);
+    }
+
+}
