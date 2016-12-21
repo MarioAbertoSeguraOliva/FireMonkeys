622f7b56c3e4977e8b30160bb590349234c84e9a
alberto <albertoesbueno@hotmail.com>
2016-12-21 00:55:06 +0000

    Pause Game
    Press Esc to enter the pause menú

---
+++ FireMonkeys/Assets/Scripts/PauseGameScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+using System.Collections;
+
+public class PauseGameScript : MonoBehaviour {
+    public GameObject ga;
+    public Animation anim;
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            PauseGame();
+	}
+
+    public void PauseGame()
+    {
+        StartCoroutine(AnimOnPaused(!ga.activeSelf));
+        if (!ga.activeSelf) TogglePause(true);
+    }
+
+    private void TogglePause(bool startPause)
+    {
+        ga.SetActive(startPause);
+        Time.timeScale = startPause ? 0.0f : 1.0f;
+        Camera.main.GetComponent<BlurOptimized>().enabled = startPause;
+    }
+
+    IEnumerator AnimOnPaused(bool startPause)
+    {
+        
+        AnimationState state = anim[anim.clip.name];
+        float startTime = Time.realtimeSinceStartup;
+        while (anim.clip.length > Time.realtimeSinceStartup - startTime)
+        {
+            anim.Play(anim.clip.name);
+
+            if(startPause)
+                state.time =  Time.realtimeSinceStartup - startTime;
+            else
+                state.time = anim.clip.length - (Time.realtimeSinceStartup - startTime);
+
+            anim.Sample();
+
+            anim.Stop();
+
+            yield return 0;
+        }
+        if (!startPause) TogglePause(false);
+    }
+}
