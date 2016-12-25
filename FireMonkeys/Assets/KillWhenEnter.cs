13384ac17fe8ca5d827bc14665aec52400f860e0
alberto <albertoesbueno@hotmail.com>
2016-12-25 02:56:20 +0000

    Moon kill
    Resolved click to retry under the gameover screen .
    Created the moon ^^

---
+++ FireMonkeys/Assets/KillWhenEnter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillWhenEnter : MonoBehaviour {
+
+    public GameObject killerPrefab;
+    private GameObject killEntity;
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Vector3 position = transform.position + Vector3.up * 40;
+            killEntity = (GameObject)Instantiate(killerPrefab, position, killerPrefab.transform.rotation);
+            killEntity.GetComponent<Moon>().objective = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(killEntity != null)
+                Destroy(killEntity);
+        }
+    }
+
+}
---
+++ FireMonkeys/Assets/Moon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moon : MonoBehaviour {
+
+    [HideInInspector]
+    public GameObject objective;
+    private float radius;
+
+	// Use this for initialization
+	void Start () {
+        radius = GetComponent<SphereCollider>().radius * transform.localScale.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(objective != null)
+        {
+            Vector3 direction = objective.transform.position - transform.position;
+            if(direction.magnitude < radius)
+            {
+                objective.GetComponent<Health>().Amount = 0;
+                Destroy(gameObject, 2f);
+            }
+            transform.position += direction.normalized * Time.deltaTime * 14;
+        }
+	}
+
+
+}
